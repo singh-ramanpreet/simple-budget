c4d969df06ef446d1e4ee40bf820008d8d01d2e4
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2024-12-23 21:27:03 +0000

    feat: update dashboard layout with improved tab navigation and fixed header

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Settings, LayoutDashboard } from "lucide-react"
+import { Settings, Home } from "lucide-react"
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -12,29 +12,27 @@ interface DashboardLayoutProps {
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   const pathname = usePathname()
 
-  const currentTab = pathname.includes("/settings")
-    ? "settings"
-    : pathname.includes("/profile")
-      ? "profile"
-      : "dashboard"
+  const currentTab = pathname.includes("/settings") ? "settings" : "dashboard"
 
   return (
     <div className="flex flex-col items-center pt-0">
-      <Tabs value={currentTab} className="w-full max-w-md overflow-hidden">
-        <TabsList className="grid h-full grid-cols-2 rounded-t-none">
-          <TabsTrigger value="dashboard" asChild>
-            <Link href="/">
-              <LayoutDashboard className="h-6 w-6" />
-            </Link>
-          </TabsTrigger>
-          <TabsTrigger value="settings" asChild>
-            <Link href="/settings">
-              <Settings size={24} />
-            </Link>
-          </TabsTrigger>
-        </TabsList>
-      </Tabs>
-      <main className="w-full">{children}</main>
+      <header className="fixed bottom-0 z-10 h-14 w-full max-w-md">
+        <Tabs value={currentTab} className="h-full overflow-hidden">
+          <TabsList className="flex h-full justify-between gap-2 rounded-none p-2 px-4">
+            <TabsTrigger value="dashboard" className="h-full" asChild>
+              <Link href="/">
+                <Home className="h-full" />
+              </Link>
+            </TabsTrigger>
+            <TabsTrigger value="settings" asChild>
+              <Link href="/settings">
+                <Settings size={24} />
+              </Link>
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </header>
+      <main className="w-full pb-14">{children}</main>
     </div>
   )
 }
